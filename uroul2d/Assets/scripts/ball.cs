56562521b0594fa2e27547d8017f09ac13fd528b
tim <llStrainll@gmail.com>
2016-04-19 23:41:54 +0300

    init

---
+++ uroul2d/Assets/scripts/ball.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+	public GameObject center;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 vector = (transform.position - center.transform.position).normalized;
+		GetComponent<Rigidbody2D>().AddForce((new Vector2(vector.y, (-1 * vector.x))));
+	}
+}
